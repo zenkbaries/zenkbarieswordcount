5721a3fc73529d5b04dc2a3354c3f81d2c76a967
John Towery <kbaries@gmail.com>
2018-11-07 16:07:29 -0700

    initial commit

---
+++ spec/zenkbarieswordcount-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ZenkbarieswordcountView from '../lib/zenkbarieswordcount-view';
+
+describe('ZenkbarieswordcountView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
